1cdb5d44e3d2f6e571e9b4f5d253f370b85c9619
zxy <1538494098@qq.com>
2019-12-31 00:03:45 +0800

    add sql ddl

--- README.md
+++ README.md
@@ -4,3 +4,16 @@
     - Mybatis
 ##参考
 [【Spring Boot 实战】论坛项目【第一季】【修正版】](https://www.baidu.com/s?wd=markdown%E9%93%BE%E6%8E%A5&f=12&rsp=0&oq=markdown%E8%BF%9E%E6%8E%A5&ie=utf-8&rsv_pq=910aa098000a85d0&rsv_t=cbe8C%2bKsW1EOSqik%2bh9oDohPUTc8UFL89u9q9C1Mu9PXsi1Lu84B6ILXJtQ&rqlang=cn)
+
+##数据库sql脚本
+```sql
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` varchar(100) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `token` char(36) DEFAULT NULL,
+  `gmt_create` bigint(20) DEFAULT NULL,
+  `gmt_modified` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1
+```
--- src/main/java/com/zxy/forum/controller/AuthorizeController.java
+++ src/main/java/com/zxy/forum/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package com.zxy.forum.controller;
 
 import com.zxy.forum.dto.AccessTokenDTO;
 import com.zxy.forum.dto.GithubUser;
+import com.zxy.forum.mapper.UserMapper;
+import com.zxy.forum.model.User;
 import com.zxy.forum.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -22,10 +27,14 @@ public class AuthorizeController {
     @Value("${github.redirect.url}")
     private String redirectUrl;
 
+    @Autowired
+    private UserMapper userMapper;
+
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -34,8 +43,22 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+
+        if(githubUser!=null) {
+            //登陆成功，写cookie和session
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            //登陆失败，，请重新登陆
+            return "redirect:/";
+        }
     }
 }
